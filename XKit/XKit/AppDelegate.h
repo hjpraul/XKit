e2ab87630a271a4ba45a8788309f55e128fa6b74
hjpraul <hjpraul@gmail.com>
2016-07-08 23:00:14 +0800

    建立工程；

---
+++ XKit/XKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XKit
+//
+//  Created by hjpraul on 16/7/8.
+//  Copyright © 2016年 hjpraul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
